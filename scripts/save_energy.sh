2a9b2a6a6ef82f8cbe20e9236032c578baac1dea
Thore Bartholomäus <thore.bar@gmail.com>
2022-05-10 22:13:31 +0200

    Refactored service and service script

--- scripts/save_energy.sh
+++ scripts/save_energy.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
-# nano /home/pi/safe_power.sh
-# chmod u+x /home/pi/safe_power.sh
 
-if [[ "$1" == "activate" ]]; then
-    # Disable Activity LED
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# Used by read_write_config_json.sh
+CONFIG_JSON_FILEPATH=$BASEDIR/../config.json
+
+source $BASEDIR/read_write_config_json.sh
+
+# Disable Activity LED
+if [ $(read_config_json disable_activity_led) = "true" ] ; then
     echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null || true
+fi
 
-    # Disable Power LED
-    # Raspberry Pi 3B / 3B+ / 4B
+# Disable Power LED
+if [ $(read_config_json disable_power_led) = "true" ] ; then
     echo 0 | sudo tee /sys/class/leds/led1/brightness > /dev/null || true
+fi
 
-    # Turn off HDMI
-    sudo /opt/vc/bin/tvservice -o || true
-
+# Disable all bluetooth services
+if [ $(read_config_json disable_bluetooth) = "true" ] ; then
     sudo systemctl disable hciuart.service || true
     sudo systemctl disable bluealsa.service || true
     sudo systemctl disable bluetooth.service || true
+fi
 
-elif [[ "$1" == "deactivate" ]]; then
-    # Activity LED
-    echo 1 | sudo tee /sys/class/leds/led0/brightness > /dev/null || true
-    # Power LED
-    echo 1 | sudo tee /sys/class/leds/led1/brightness > /dev/null || true
-    # HDMI
-    sudo /opt/vc/bin/tvservice -p || true
-
-    sudo systemctl enable hciuart.service || true
-    sudo systemctl enable bluealsa.service || true
-    sudo systemctl enable bluetooth.service || true
+# Disable HDMI
+if [ $(read_config_json disable_hdmi) = "true" ] ; then
+    sudo tvservice --off || true
 fi
