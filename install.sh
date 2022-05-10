817c8561e5753b6da18308e5f2241b1692db4d81
Thore Bartholomäus <thore.bar@gmail.com>
2022-05-10 23:25:08 +0200

    - Use /boot/config.txt for PWR and ACT LED - Check Pi model for ethernet LEDs - Refactoring and fine tuning

--- install.sh
+++ install.sh
@@ -18,43 +18,61 @@ restart=false
 needs_service=false
 
 #
-# Activity LED
-#
-
-disable_activity_led()
-{
-    write_config_json disable_activity_led true
-    needs_service=true
-}
-
-user_yes_no "Do you want to disable the Activity LED?" disable_activity_led
-
-#
 # Power LED
 #
 
 disable_power_led()
 {
     write_config_json disable_power_led true
-    needs_service=true
+
+    write_boot_config "" true dtparam pwr_led_trigger none
+    write_boot_config "" true dtparam pwr_led_activelow off
+    restart=true
 }
 
 user_yes_no "Do you want to disable the Power LED?" disable_power_led
 
 #
-# Ethernet LEDs
+# Activity LED
 #
 
-disable_ethernet_leds()
+disable_activity_led()
 {
-    write_config_json disable_ethernet_leds true
+    write_config_json disable_activity_led true
 
-    write_boot_config "" true dtparam eth_led0 14
-    write_boot_config "" true dtparam eth_led1 14
+    write_boot_config "" true dtparam act_led_trigger none
+    write_boot_config "" true dtparam act_led_activelow off
     restart=true
 }
 
-user_yes_no "Do you want to disable the Ethernet LEDs?" disable_ethernet_leds
+user_yes_no "Do you want to disable the Activity LED?" disable_activity_led
+
+#
+# Ethernet LEDs
+#
+
+# https://github.com/raspberrypi/firmware/blob/master/boot/overlays/README
+ethernet_off=0
+model=$(tr -d '\0' < /proc/device-tree/model)
+if [[ $model == *"3 Model B Plus"* ]]; then
+    ethernet_off=14
+elif [[ $model == *"4 Model B"* ]]; then
+    ethernet_off=4
+fi
+
+if [ $ethernet_off -ne 0 ]; then
+
+    disable_ethernet_leds()
+    {
+        write_config_json disable_ethernet_leds true
+
+        write_boot_config "" true dtparam eth_led0 $ethernet_off
+        write_boot_config "" true dtparam eth_led1 $ethernet_off
+        restart=true
+    }
+
+    user_yes_no "Do you want to disable the Ethernet LEDs?" disable_ethernet_leds
+fi
 
 #
 # Bluetooth
@@ -65,8 +83,10 @@ disable_bluetooth()
     write_config_json disable_bluetooth true
 
     write_boot_config "" false dtoverlay disable-bt
+    stop_service hciuart.service
+    stop_service bluealsa.service
+    stop_service bluetooth.service
     restart=true
-    needs_service=true
 }
 
 user_yes_no "Do you want to disable Bluetooth?" disable_bluetooth
@@ -102,12 +122,12 @@ disable_hdmi()
 user_yes_no "Do you want to disable HDMI?" disable_hdmi
 
 
-if [ "$needs_service" = true ] ; then
+if [ "$needs_service" = true ]; then
     setup_service $BASEDIR/services/save_energy.service $BASEDIR
     start_service save_energy.service
 fi
 
-if [ "$restart" = true ] ; then
+if [ "$restart" = true ]; then
     reboot()
     {
         sudo reboot now
